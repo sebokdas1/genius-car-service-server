15e9ed2080320e033e9f8c2619a296c1ab2f052a
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-22 21:09:34 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+install all node and mongo and other: 
+npm init -y
+npm i express cors mongodb dotenv
+for run: npm run start-dev
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('running car server');
+});
+
+app.listen(port, () => {
+    console.log('listening port', port);
+});
