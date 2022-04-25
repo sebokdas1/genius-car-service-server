79ae0c7293aaa3ea6516793cf5775cb83bf5ba8d
sebokdas1 <sebok.das.ff@gmail.com>
2022-04-25 11:08:31 +0600

    getting order personal info by email and installed jwt

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
         });
 
         //Order collection API
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
